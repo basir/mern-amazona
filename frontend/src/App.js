6019d933251837d4e0bd691bb1e3cb4b1072df6a
Basir <basir.jafarzadeh@gmail.com>
2021-12-28 14:38:27 +0330

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
