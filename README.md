25ced0c78bd79bbe15b2bf189c7c0de5a976c415
Basir <basir.jafarzadeh@gmail.com>
2021-12-28 22:53:20 +0330

    Video-05-List-Products

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 2. Install Tools
 3. Create React App
 4. Create Git Repository
+5. List Products
+   1. create products array
+   2. add product images
+   3. render products
+   4. style products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
