0147c6af257f45fabfd7917ae2f2c15c1875ed45
Basir <basir.jafarzadeh@gmail.com>
2022-01-05 13:56:34 +0330

    Video-19-Connect-To-MongoDB-Database

--- README.md
+++ README.md
@@ -73,3 +73,8 @@
     1. create sign in form
     2. add email and password
     3. add signin button
+19. Connect To MongoDB Database
+    1. create atlas monogodb database
+    2. install local mongodb database
+    3. npm install mongoose
+    4. connect to mongodb database
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 // test
