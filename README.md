b76a5eaf8450ee795061c33f3e28dab384c99aa3
Basir <basir.jafarzadeh@gmail.com>
2022-01-05 17:56:14 +0330

    Video-21.-Seed-Sample-Users

--- README.md
+++ README.md
@@ -83,3 +83,6 @@
     2. create seed route
     3. use route in server.js
     4. seed sample product
+21. Seed Sample Users
+    1. create user model
+    2. seed sample users
--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
   await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 export default seedRouter;
