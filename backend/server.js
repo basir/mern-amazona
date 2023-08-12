c7c10b0fa8765fea96a96110a4aae25a8314cd00
root <root@ip-172-31-84-42.ec2.internal>
2023-08-12 08:17:14 +0000

    server url fix

--- backend/server.js
+++ backend/server.js
@@ -42,9 +42,9 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
