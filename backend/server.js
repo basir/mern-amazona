3f71ed2aee43600224d1405134171f5660e27112
octa230 <mervynstunner@outlook.com>
2023-08-14 15:05:24 +0300

    production nenv

--- backend/server.js
+++ backend/server.js
@@ -41,12 +41,14 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) =>
+if(process.env.NODE_ENV === 'production'){
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
 );
 
+}
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
